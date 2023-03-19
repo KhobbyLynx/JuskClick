4a9ec6083d6724076cbddc00ad99a650bb6afeb3
Samuel Tetteh <khobbylynx55@gmail.com>
2023-03-19 07:48:01 +0000

    Navbar Update

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,10 +2,12 @@ import {useEffect, useState} from 'react'
 import './Navbar.scss'
 import logo from '../../assets/logo.png'
 import { Link } from 'react-router-dom'
+import { AiOutlineMenuUnfold, AiOutlineClose } from 'react-icons/ai'
 
 const Navbar = () => {
   //This state Changes the navbar view on scroll
   const [active, setActive] = useState(false)
+  const [toggle, setToggle ] = useState(false)
   
   const isActive = () => {
     //On vertical scroll, the state of active is set to true
@@ -20,6 +22,14 @@ const Navbar = () => {
     }
   },[])
 
+  function addNav() {
+    setToggle(true)
+  }
+
+  function removeNav() {
+    setToggle(false)
+
+  }
   let currentUser= {
     id: 1,
     username: "Samuel Tetteh",
@@ -31,57 +41,137 @@ const Navbar = () => {
   return (
     <div className={active ? "navbar active" : "navbar"}>
       <div className="container">
+
+        { toggle ?
+          <AiOutlineClose className='toggle--nav-icon' onClick={removeNav}/>:
+          <AiOutlineMenuUnfold className='toggle--nav-icon' onClick={addNav}/> 
+        }
+
         <div className="logo">
           <Link to='/' className='link'>
             <img src={logo} alt="juskclick logo"/>
           </Link>
         </div>
         <nav>
-          {["How It Works", "Post A Job", "Hire A Professional", "About Us", "Why JuskClick"].map(link => (
-              <Link to={link} className="link">
-                <span key={link}>{link}</span>
-              </Link>
-          ))}
+          <Link to='how-it-works' className='link' >How It Works</Link>
+          <Link to='post-a-job' className='link' >Post A Job</Link>
+          <Link to='hire-a-professional' className='link' >Hire A Professional</Link>
+          <Link to='about-us' className='link' >About Us</Link>
+          <Link to='why-juskclick' className='link' >Why JuskClick</Link>
         </nav>
         <div className="account">
           {!currentUser &&
           <>
-            <span className='login'>Log In</span>
-            <button>Sign In</button>
+            <Link to='' className='login link'>
+              Log In
+            </Link>
+            <Link to='' className='link'>
+              <button>Sign In</button>
+            </Link>   
           </>
           }
           {currentUser && 
           <div className='user'>
             <img src="https://samueltetteh.netlify.app/static/media/aboutImg.b2c6393a6bb40b3e6d60.jpg" alt="user image" />
-            <span>{currentUser.username}</span>
+            <span className='username'>{currentUser.username}</span>
             <div className="options">
               {currentUser?.isSeller &&
                 (
                   <>
-                    <Link to='mygigs' className='link'><span>Gigs</span></Link>
-                    <Link to='add' className='link'><span>Add New Gig</span></Link>
+                    <Link to='mygigs' className='link span'>
+                      My Gigs
+                    </Link>
+                    <Link to='add' className='link span'>
+                      Add New Gig
+                    </Link>
                   </>
                 )
               }
-              <Link to='orders' className='link'><span>Orders</span></Link>
-              <Link to='messages' className='link'><span>Messages</span></Link>
-              <Link to='login' className='link'><span>Log Out</span></Link>
+              <Link to='orders' className='link span'>
+                Orders
+              </Link>
+              <Link to='messages' className='link span'>
+                Messages
+              </Link>
+              <Link to='login' className='link span'>
+                Log Out
+              </Link>
             </div>
           </div>
           }
         </div>
       </div>
 
+      
+      
       {active && (
         <>
           <hr />
           <div className="menu">
-            <span>Cat 1</span>
-            <span>Cat 1</span>
-            <span>Cat 1</span>
+            <span>Web Development</span>
+            <span>Graphic Design</span>
+            <span>Photography</span>
           </div>
         </>
       )}
+
+      { toggle && 
+        <nav className='toggle--nav'>
+          { currentUser &&
+            <div className='user'>
+              <img src="https://samueltetteh.netlify.app/static/media/aboutImg.b2c6393a6bb40b3e6d60.jpg" alt="user image" />
+              <span className='username'>{currentUser.username}</span>
+              { currentUser?.isSeller ?
+                <span className='freelancer'>Freelancer</span> :
+                <span className='freelancer'>Client</span>
+              }
+            </div>
+          }
+          {!currentUser ? 
+          (
+            <>
+              <Link to='how-it-works' className='link' >How It Works</Link>
+              <hr />
+              <Link to='post-a-job' className='link' >Post A Job</Link>
+              <hr />
+              <Link to='hire-a-professional' className='link' >Hire A Professional</Link>
+              <hr />
+              <Link to='about-us' className='link' >About Us</Link>
+              <hr />
+              <Link to='why-juskclick' className='link' >Why JuskClick</Link>
+            </>
+          )
+          :
+          (
+            <>
+              <Link to='orders' className='link' >Orders</Link>
+              <hr />
+              <Link to='messages' className='link' >Messages</Link>
+              <hr />
+            {currentUser?.isSeller ? (     
+              <>
+              <Link to='mygigs' className='link' >My Gigs</Link>
+              <hr />
+              <Link to='add' className='link' >Add New Gig</Link>
+              </>
+            ): 
+            (
+              <>
+              <Link to='post-a-job' className='link' >Post A Job</Link>
+              </>
+            )
+            }
+            <hr />
+              <Link to='why-juskclick' className='link' >Why JuskClick</Link>
+              <Link to='login' className='link btn' >
+                <button>Log Out</button>
+              </Link>
+            </>
+          )
+          }
+        </nav>
+      }
+
     </div>
   )
 }
